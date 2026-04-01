e313cfae6ccae963ec9c8445c81e000fa1904268
Jiang Haodi <jhd24@mails.tsinghua.edu.cn>
2026-04-01 03:41:36 +0000

    [feat] Compatibility with PipeANN.

--- src/pq_search.cu
+++ src/pq_search.cu
@@ -29,8 +29,11 @@ namespace gustann {
     // read data from file
     DEBUG("Reading metadata");
     read_bin(table_file, nr, nc, 0, basic_offsets);
-    ASSERT(nr == 4 && nc == 1);
-    DEBUG("Metadata: {} {} {} {}", basic_offsets[0], basic_offsets[1], basic_offsets[2], basic_offsets[3]);
+    ASSERT((nr == 4 || nr == 5) &&
+           nc == 1); // PipeANN (and older version of DiskANN) use (nr == 5),
+                     // while new verison of DiskANN use nr == 4.
+    
+    DEBUG("Metadata: {} {} {} {} {}", basic_offsets[0], basic_offsets[1], basic_offsets[2], basic_offsets[3], (nr == 5 ? basic_offsets[4] : basic_offsets[3]));
 
     DEBUG("Reading pivots");
     read_bin(table_file, nr, host_data.dim, basic_offsets[0], host_data.pivots);
@@ -45,9 +48,16 @@ namespace gustann {
     read_bin(table_file, nr, nc, basic_offsets[1], host_data.centroid);
     ASSERT(nr == host_data.dim && nc == 1);
 
+    // Older version of DiskANN have the `rearrangement field` in
+    // `basic_offsets[2]`;
+    // For compatibility, PipeANN still contains this field.
+    // Therefore for new DiskANN (nr == 4), chunk offsets is in basic_offset[2]
+    // but in PipeANN, it's in basic_offset[3]
+    int chunk_offset_idx = nr == 4 ? 2 : 3;
+    
     DEBUG("Reading chunk offsets");
     int* chunk_offsets;
-    read_bin(table_file, nr, nc, basic_offsets[2], chunk_offsets);
+    read_bin(table_file, nr, nc, basic_offsets[chunk_offset_idx], chunk_offsets);
     ASSERT(nr == host_data.num_chunks + 1 && nc == 1);
     host_data.chunk_id = new int[host_data.dim];
     memset(host_data.chunk_id, -1, sizeof(int) * host_data.dim);
